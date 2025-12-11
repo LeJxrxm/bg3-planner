882f98e5f74b4734720c6d7ff856c48285004a7b
jerem <jeremy.ounnas@outlook.com>
2025-12-11 11:21:53 +0100

    Add image field to Character, Merchant, Quest, Poi, and Npc models

---
+++ prisma/migrations/20251211102037_add_images/migration.sql
@@ -0,0 +1,14 @@
+-- AlterTable
+ALTER TABLE "Character" ADD COLUMN     "image" TEXT;
+
+-- AlterTable
+ALTER TABLE "Merchant" ADD COLUMN     "image" TEXT;
+
+-- AlterTable
+ALTER TABLE "Npc" ADD COLUMN     "image" TEXT;
+
+-- AlterTable
+ALTER TABLE "Poi" ADD COLUMN     "image" TEXT;
+
+-- AlterTable
+ALTER TABLE "Quest" ADD COLUMN     "image" TEXT;
